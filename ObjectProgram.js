e5bcb615e41a013e1062c135b8e1144c009db13d
ShobikaSenthil <90506704+ShobikaSenthil@users.noreply.github.com>
2022-10-25 20:04:57 +0530

    Add files via upload

---
+++ ObjectProgram.js
@@ -0,0 +1,35 @@
+var obj = {
+    a: {
+        b: {
+            c: 12,
+            j: false
+        },
+        k: null
+    }
+};
+
+const findPath = (object, path) => {
+    let temp=object;
+    let str=path.split('.');
+    
+    
+    
+    for(let i=0;i<str.length;i++){
+      
+             temp=temp[str[i]]
+             if(temp===undefined){
+                 return undefined
+             }
+
+}
+    return temp;
+};
+console.log(findPath(obj, 'a.b.c')); // 12
+console.log(findPath(obj, 'a.b')); // {c: 12, j: false}
+console.log(findPath(obj, 'a.b.d')); // undefined
+console.log(findPath(obj, 'a.c')); // undefined
+console.log(findPath(obj, 'a.b.c.d')); // undefined
+console.log(findPath(obj, 'a.b.c.d.e')); // undefined
+console.log(findPath(obj, 'a.b.j')); //false
+console.log(findPath(obj, 'a.b.j.k')); //undefined
+console.log(findPath(obj, 'a.k')); //null
